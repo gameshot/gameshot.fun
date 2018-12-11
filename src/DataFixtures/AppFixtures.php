80474e0a465b37e6bee637325cc9a0211de6d664
PStefano <petruzzelli_stefano@hotmail.com>
2018-12-11 14:44:14 +0100

    Stefano commit first part of the forum (category display)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,15 +8,19 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Role;
+use App\Entity\Topic;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\ExpressionLanguage\Tests\Node\Obj;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $this->loadRoles($manager);
+        $this->loadCategories($manager);
 
         $manager->flush();
     }
@@ -34,4 +38,31 @@ class AppFixtures extends Fixture
             $manager->persist($role);
         }
     }
+
+    public function loadCategories(ObjectManager $manager)
+    {
+        $categoryList = [
+            'Gameshot discussion',
+            'Game discussions'
+        ];
+        $topicList = [
+            'Gameshot',
+            'Gameshot bugs'
+        ];
+
+        foreach ($categoryList as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            foreach ($topicList as $topicName)
+            {
+                $topic = new Topic();
+                $topic->setName($topicName);
+                $topic->setCategory($category);
+                $manager->persist($topic);
+                $category->addTopic($topic);
+            }
+            $manager->persist($category);
+        }
+    }
+
 }
