a5df097bf89d6b563911c9d0e5a61d1e9bed594a
xampocode <42768052+xampocode@users.noreply.github.com>
2018-12-13 11:51:32 +0100

    game auto smooth scrool

---
+++ public/js/gamescroll.js
@@ -0,0 +1,5 @@
+/* When the user clicks on the submit to play the game button, scroll's to the top and centers the game on screen */
+function topFunction() {
+    document.body.scrollTop = 100; // For Safari
+    document.documentElement.scrollTop = 100; // For Chrome, Firefox, IE and Opera
+}
