1403b08bcc31275d87a58dac042b8fe712fae7e6
xampocode <42768052+xampocode@users.noreply.github.com>
2018-12-13 16:45:23 +0100

    admin, user and role pages and controllers

---
+++ src/Controller/RoleController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/* controller for role page JP */
+
+class RoleController extends Controller
+{
+
+    /**
+     *
+     * @Route("admin/user/role", name="user/role")
+     *
+     */
+    public function role(){
+        return $this->render('user/Role.html.twig');
+    }
+}
+
