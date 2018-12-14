47921e00e2510c35156a5bb8f111f4ea44936d03
xampocode <42768052+xampocode@users.noreply.github.com>
2018-12-14 23:10:29 +0100

    css and html comments added and indentation review

--- public/js/gamescroll.js
+++ public/js/gamescroll.js
@@ -1,4 +1,4 @@
-/* When the user clicks on the submit to play the game button, scroll's to the top and centers the game on screen */
+/* When the user clicks on the submit to play the game button, scroll's to the top and centers the game on screen JP */
 function topFunction() {
     document.body.scrollTop = 100; // For Safari
     document.documentElement.scrollTop = 100; // For Chrome, Firefox, IE and Opera
